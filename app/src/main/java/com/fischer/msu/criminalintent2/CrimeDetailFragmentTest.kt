7ebd60ff4676f118e038b81dc27584c7bbe503ea
Melaina Fischer <melainafischer@gmail.com>
2023-05-01 14:19:59 -0500

    Ch 9 completed

---
+++ app/src/main/java/com/fischer/msu/criminalintent2/CrimeDetailFragmentTest.kt
@@ -0,0 +1,47 @@
+package com.fischer.msu.criminalintent2
+
+import androidx.core.view.isVisible
+import androidx.fragment.app.testing.FragmentScenario
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+class CrimeDetailFragmentTest {
+
+    private lateinit var scenario: FragmentScenario<CrimeDetailFragment>
+
+    @Before
+    fun setUp() {
+        scenario = launchFragmentInContainer()
+    }
+
+    @After
+    fun tearDown() {
+        scenario.close()
+    }
+
+    @Test
+    fun checkBoxStateEqualsCrimeSolvedState() {
+        scenario.onFragment { fragment ->
+            // Check that the checkbox is visible
+            onView(withId(R.id.crime_solved)).check(matches(isVisible()))
+
+            // Check that the checkbox is unchecked by default
+            onView(withId(R.id.crime_solved)).check(matches(isNotChecked()))
+
+            // Simulate clicking the checkbox to mark the crime as solved
+            onView(withId(R.id.crime_solved)).perform(click())
+
+            // Check that the checkbox is now checked
+            onView(withId(R.id.crime_solved)).check(matches(isChecked()))
+
+            // Check that the crime is now marked as solved
+            assert(fragment.crime.isSolved)
+        }
+    }
+}
---
+++ app/src/main/java/com/fischer/msu/criminalintent2/CrimeListFragment.kt
@@ -0,0 +1,18 @@
+package com.fischer.msu.criminalintent2
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+
+private const val TAG = "CrimeListFragment"
+
+class CrimeListFragment: Fragment() {
+
+    private val crimeListViewModel :CrimeListViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "total Crimes : ${crimeListViewModel.crimes.size}")
+    }
+
+}
