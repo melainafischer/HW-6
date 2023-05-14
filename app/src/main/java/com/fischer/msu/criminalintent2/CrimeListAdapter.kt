15b1aa05b0f8efb36f78369bb438738b44ace70a
Melaina Fischer <melainafischer@gmail.com>
2023-05-14 13:59:54 -0500

    finished 3A

--- app/src/main/java/com/fischer/msu/criminalintent2/CrimeListAdapter.kt
+++ app/src/main/java/com/fischer/msu/criminalintent2/CrimeListAdapter.kt
@@ -1,6 +1,7 @@
 package com.fischer.msu.criminalintent2
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
@@ -20,8 +21,15 @@ class CrimeHolder (
                 Toast.LENGTH_SHORT
             ).show()
         }
+
+        binding.crimeSolved.visibility = if (crime.isSolved) {
+            View.VISIBLE
+            }
+        else{
+            View.GONE
+            }
+        }
     }
-}
 
 class CrimeListAdapter (private val crimes: List<Crime>): RecyclerView.Adapter<CrimeHolder>(){
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CrimeHolder {
